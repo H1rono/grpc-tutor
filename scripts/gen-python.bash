cfd8db0adc5568753bac78266337c0e2e06693b9
H1rono <hronok66@gmail.com>
2024-06-19 07:30:57 +0900

    :wrench: Add script to generate python code

---
+++ scripts/gen-python.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eux -o pipefail
+
+for file in $(ls protos)
+do
+	: "process $file"
+	rye run \
+		python3 -m \
+		grpc_tools.protoc \
+		-I./protos \
+		--python_out=./py --grpc_python_out=./py \
+		--mypy_out=./py --mypy_grpc_out=./py \
+		--plugin=protoc-gen-mypy=./.venv/bin/protoc-gen-mypy \
+		--plugin=ptotoc-gen-mypy_grpc=./.venv/bin/protoc-gen-mypy_grpc \
+		"$file"
+done
