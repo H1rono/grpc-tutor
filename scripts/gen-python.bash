f5eef8b2dfc5ca975b04475ea2c94c4d8ded8c51
H1rono <hronok66@gmail.com>
2024-06-19 07:38:54 +0900

    :wrench: Change destination of generated python codes

--- scripts/gen-python.bash
+++ scripts/gen-python.bash
@@ -8,10 +8,10 @@ do
 	rye run \
 		python3 -m \
 		grpc_tools.protoc \
-		-I./protos \
+		-Igenerated=./protos \
 		--python_out=./py --grpc_python_out=./py \
 		--mypy_out=./py --mypy_grpc_out=./py \
 		--plugin=protoc-gen-mypy=./.venv/bin/protoc-gen-mypy \
 		--plugin=ptotoc-gen-mypy_grpc=./.venv/bin/protoc-gen-mypy_grpc \
-		"$file"
+		"./protos/$file"
 done
