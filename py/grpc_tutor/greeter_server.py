e3a526846e862187c9f88111078522dc8ab8220f
H1rono <hronok66@gmail.com>
2024-06-19 06:49:15 +0900

    :art: Fmt

--- py/grpc_tutor/greeter_server.py
+++ py/grpc_tutor/greeter_server.py
@@ -10,7 +10,9 @@ import helloworld_pb2_grpc
 
 
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
-    def SayHello(self, request: helloworld_pb2_grpc.HelloRequest, context: grpc.ServicerContext) -> helloworld_pb2.HelloReply:
+    def SayHello(
+        self, request: helloworld_pb2_grpc.HelloRequest, context: grpc.ServicerContext
+    ) -> helloworld_pb2.HelloReply:
         return helloworld_pb2.HelloReply(message="Hello, %s!" % request.name)
 
 
