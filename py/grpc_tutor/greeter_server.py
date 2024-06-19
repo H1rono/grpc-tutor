840b126eeaafffa739bf8f41b44adf2757c20835
H1rono <hronok66@gmail.com>
2024-06-20 05:07:30 +0900

    :sparkles: Add async helloworld server

--- py/grpc_tutor/greeter_server.py
+++ py/grpc_tutor/greeter_server.py
@@ -1,10 +1,11 @@
 # https://github.com/grpc/grpc/blob/b8a04ac/examples/python/helloworld/greeter_server.py
 """The Python implementation of the GRPC helloworld.Greeter server."""
 
+import asyncio
 import logging
 from concurrent import futures
 
-import grpc
+import grpc.aio
 from generated import helloworld_pb2, helloworld_pb2_grpc
 
 
@@ -15,6 +16,15 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
         return helloworld_pb2.HelloReply(message="Hello, %s!" % request.name)
 
 
+class AsyncGreeter(helloworld_pb2_grpc.GreeterServicer):
+    async def SayHello(
+        self, request: helloworld_pb2.HelloRequest, context: grpc.ServicerContext
+    ) -> helloworld_pb2.HelloReply:
+        print(type(context))
+        await asyncio.sleep(0.1)
+        return helloworld_pb2.HelloReply(message=f"[async] Hello, {request.name}!")
+
+
 def serve() -> None:
     port = "50051"
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -23,6 +33,26 @@ def serve() -> None:
     server.start()
     print("Server started, listening on " + port)
     server.wait_for_termination()
+
+
+async def start_server(server: grpc.aio.Server) -> None:
+    port = "50051"
+    server = grpc.aio.server(futures.ThreadPoolExecutor(max_workers=10))
+    helloworld_pb2_grpc.add_GreeterServicer_to_server(AsyncGreeter(), server)
+    server.add_insecure_port(f"[::]:{port}")
+    await server.start()
+    print(f"Server started, listening on port {port}")
+    await server.wait_for_termination()
+
+
+def run_async_serve() -> None:
+    loop = asyncio.get_event_loop()
+    server = grpc.aio.server(futures.ThreadPoolExecutor(max_workers=10))
+    try:
+        loop.run_until_complete(start_server(server))
+    finally:
+        loop.run_until_complete(server.stop(1))
+        loop.close()
 
 
 if __name__ == "__main__":
