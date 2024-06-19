60fb99997ea6731d2ab6b8ab647ae5e17199d85f
H1rono <hronok66@gmail.com>
2024-06-19 23:58:42 +0900

    :sparkles: Add helloworld async client

--- py/grpc_tutor/greeter_client.py
+++ py/grpc_tutor/greeter_client.py
@@ -3,9 +3,10 @@
 
 from __future__ import print_function
 
+import asyncio
 import logging
 
-import grpc
+import grpc.aio
 from generated import helloworld_pb2, helloworld_pb2_grpc
 
 
@@ -18,6 +19,18 @@ def run() -> None:
         stub = helloworld_pb2_grpc.GreeterStub(channel)
         response = stub.SayHello(helloworld_pb2.HelloRequest(name="you"))
     print("Greeter client received: " + response.message)
+
+
+async def client_async() -> None:
+    print("Will try to greet world ...")
+    async with grpc.aio.insecure_channel("localhost:50051") as channel:
+        stub = helloworld_pb2_grpc.GreeterStub(channel)
+        response = await stub.SayHello(helloworld_pb2.HelloRequest(name="world"))
+    print("Greeter client received: " + response.message)
+
+
+def run_async() -> None:
+    asyncio.run(client_async())
 
 
 if __name__ == "__main__":
