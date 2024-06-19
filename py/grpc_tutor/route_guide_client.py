45f1b43c1ea17448e8aa56a35f7009c9951ec799
H1rono <hronok66@gmail.com>
2024-06-19 09:43:48 +0900

    :sparkles: Impl ListFeatures in client

--- py/grpc_tutor/route_guide_client.py
+++ py/grpc_tutor/route_guide_client.py
@@ -4,13 +4,34 @@ import grpc
 from generated import route_guide_pb2, route_guide_pb2_grpc
 
 
+def get_feature(stub: route_guide_pb2_grpc.RouteGuideStub) -> None:
+    feature = stub.GetFeature(
+        route_guide_pb2.Point(latitude=356810420, longitude=1397672140)
+    )
+    assert isinstance(feature, route_guide_pb2.Feature)
+    if not feature.name:
+        print("received no feature")
+    else:
+        print(f"received feature name: {feature.name}")
+
+
+def list_features(stub: route_guide_pb2_grpc.RouteGuideStub) -> None:
+    rectangle = route_guide_pb2.Rectangle(
+        lo=route_guide_pb2.Point(latitude=350000000, longitude=1390000000),
+        hi=route_guide_pb2.Point(latitude=360000000, longitude=1400000000),
+    )
+    for feature in stub.ListFeatures(rectangle):
+        assert isinstance(feature, route_guide_pb2.Feature)
+        print(f"received feature name: {feature.name}")
+
+
 def run() -> None:
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = route_guide_pb2_grpc.RouteGuideStub(channel)
-        response = stub.GetFeature(
-            route_guide_pb2.Point(latitude=356810420, longitude=1397672140)
-        )
-    print("Route guide client received: " + response.name)
+        print("----- GetFeature -----")
+        get_feature(stub)
+        print("----- ListFeatures -----")
+        list_features(stub)
 
 
 if __name__ == "__main__":
