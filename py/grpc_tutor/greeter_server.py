0b387e51f220602389abab38d5a3a6d46f7a4dc9
H1rono <hronok66@gmail.com>
2024-06-19 23:58:28 +0900

    :white_check_mark: Fix

--- py/grpc_tutor/greeter_server.py
+++ py/grpc_tutor/greeter_server.py
@@ -10,7 +10,7 @@ from generated import helloworld_pb2, helloworld_pb2_grpc
 
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
     def SayHello(
-        self, request: helloworld_pb2_grpc.HelloRequest, context: grpc.ServicerContext
+        self, request: helloworld_pb2.HelloRequest, context: grpc.ServicerContext
     ) -> helloworld_pb2.HelloReply:
         return helloworld_pb2.HelloReply(message="Hello, %s!" % request.name)
 
