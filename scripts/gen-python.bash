6a3b81f70ca103e02f9ce156cfbcd56e7dc98fef
H1rono <hronok66@gmail.com>
2024-06-22 00:12:01 +0900

    :wrench: Change generated file destination

--- scripts/gen-python.bash
+++ scripts/gen-python.bash
@@ -5,10 +5,13 @@ set -eux -o pipefail
 for file in $(ls protos)
 do
 	: "process $file"
+	pkg_name=`cat "./protos/$file" | grep package | head -n 1 | sed -r 's/^package\s+(.*)\s*;$/\1/'`
+	mkdir -p "./py/$pkg_name"
+	touch "./py/$pkg_name/__init__.py"
 	rye run \
 		python3 -m \
 		grpc_tools.protoc \
-		-Igenerated=./protos \
+		-I${pkg_name}=./protos \
 		--python_out=./py \
 		--pyi_out=./py \
 		--grpc_python_out=./py \
