3676bf9c1aab733094ef121c9c6d772d9a1c6c71
H1rono <hronok66@gmail.com>
2025-01-18 06:46:21 +0900

    :recycle: Derive serde traits

--- rs/routeguide/build.rs
+++ rs/routeguide/build.rs
@@ -1,4 +1,12 @@
+const PROTO_FILE: &str = "../../protos/route_guide.proto";
+const PROTO_DIR: &str = "../../protos";
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../../protos/route_guide.proto")?;
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .build_transport(true)
+        .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
+        .compile_protos(&[PROTO_FILE], &[PROTO_DIR])?;
     Ok(())
 }
